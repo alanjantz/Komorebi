c11f008daad65cd5d8efc397461f40f811861bba
Alan Jantz <kelphyz@gmail.com>
2019-03-27 21:15:39 -0300

    Aparência modal

--- content/komorebi.js
+++ content/komorebi.js
@@ -5,9 +5,11 @@ var element = document.getElementById(elementId);
     for (var i = 0; i < number; i++) 
     {
         var div = document.createElement("div");
-        div.innerHTML += '<button class="anime-button not-growing-padding transation-effect-05s" onclick="trazerInformacoes(\'p\/1.html\')"><img src="http://i.imgur.com/wHAtAJ4.png"/></button>';
+        div.innerHTML += `<button class="anime-button not-growing-padding transation-effect-05s" onclick="trazerInformacoes(\'p\/1.html\')">
+                            <img class="transation-effect-1s" src="http://i.imgur.com/wHAtAJ4.png"/>
+                          </button>`;
         div.setAttribute('class', 'list-item not-growing-padding');
-        console.log(div);
+
         element.appendChild(div);
     }
 }
