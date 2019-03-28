362637235c5e1b82151ab45408353dd1520bb50f
Alan Jantz <kelphyz@gmail.com>
2019-03-27 22:19:15 -0300

    Minificação para modal e página de detalhes

--- content/details.js
+++ content/details.js
@@ -1,12 +1,21 @@
 function getParameterByName(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    const param = urlParams.get(name);
+    var urlParams = new URLSearchParams(window.location.search);
+    var param = urlParams.get(name);
 
     return param;
 }
 
 function redirectToAnime(name) {
-    var newUrl = 'p/' + getParameterByName(name) + '.html';
+    var newUrl = 'p/' + getParameterByName(name);
+    
+    if (!newUrl.endsWith('.html'))
+        newUrl += '.html'
+    
     var iframeElement = document.getElementById('iframeDetails');
+
+    if (getParameterByName('min') != null && getParameterByName('min') != undefined && getParameterByName('min') == 1)
+        newUrl += "?min=1"
+
     iframeElement.src = newUrl;
+
 }
--- content/komorebi.js
+++ content/komorebi.js
@@ -5,7 +5,7 @@ var element = document.getElementById(elementId);
     for (var i = 0; i < number; i++) 
     {
         var div = document.createElement("div");
-        div.innerHTML += `<button class="anime-button not-growing-padding transation-effect-05s" onclick="trazerInformacoes('details.html?anime=1')">
+        div.innerHTML += `<button class="anime-button not-growing-padding transation-effect-05s" onclick="trazerInformacoes('details.html?anime=1.html&min=1')">
                             <img class="transation-effect-1s" src="http://i.imgur.com/wHAtAJ4.png"/>
                           </button>`;
         div.setAttribute('class', 'list-item not-growing-padding');
