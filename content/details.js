d22a34f795bd252afa0ca1b982c69ad61bfd414e
Alan Jantz <kelphyz@gmail.com>
2019-03-30 17:35:31 -0300

    Organização de fluxo

--- content/details.js
+++ content/details.js
@@ -7,15 +7,56 @@ function getParameterByName(name) {
 
 function redirectToAnime(name) {
     var newUrl = 'p/' + getParameterByName(name);
-    
+
     if (!newUrl.endsWith('.html'))
         newUrl += '.html'
-    
+
     var iframeElement = document.getElementById('iframeDetails');
 
     if (getParameterByName('min') != null && getParameterByName('min') != undefined && getParameterByName('min') == 1)
         newUrl += "?min=1"
 
     iframeElement.src = newUrl;
+}
 
+function createTable(paramName) {
+    var anime = getParameterByName(paramName);
+
+    console.log(anime);
+
+    var object = [
+        {
+            "Ep": 1,
+            "Nome": "rooter",
+            "Assistido": "12345",
+        },
+        {
+            "Ep": 2,
+            "Nome": "rooter",
+            "Assistido": "12345",
+        },
+        {
+            "Ep": 3,
+            "Nome": "rooter",
+            "Assistido": "12345",
+        },
+        {
+            "Ep": 4,
+            "Nome": "rooter",
+            "Assistido": "12345",
+        }
+    ];
+
+    $('#content').append('<table id="episodiosList"><thead><tr></tr></thead><tbody></tbody></table>');
+
+    $.each(object, function (index, jsonObject) {
+        if (Object.keys(jsonObject).length > 0) {
+            var tableRow = '<tr>';
+            $.each(Object.keys(jsonObject), function (i, key) {
+                tableRow += '<td>' + jsonObject[key] + '</td>';
+            });
+            tableRow += "</tr>";
+            $('#episodiosList tbody').append(tableRow);
+        }
+    });
 }
--- content/komorebi.js
+++ content/komorebi.js
@@ -1,15 +1,13 @@
-function appendChild(elementId, number)
-{
-var element = document.getElementById(elementId);
+function appendChild(elementId, number) {
+  var element = document.getElementById(elementId);
 
-    for (var i = 0; i < number; i++) 
-    {
-        var div = document.createElement("div");
-        div.innerHTML += `<button class="anime-button not-growing-padding transation-effect-05s" onclick="trazerInformacoes('details.html?anime=1.html&min=1')">
+  for (var i = 0; i < number; i++) {
+    var div = document.createElement("div");
+    div.innerHTML += `<button class="anime-button not-growing-padding transation-effect-05s" onclick="trazerInformacoes('details.html?anime=example&min=1')">
                             <img class="transation-effect-1s" src="http://i.imgur.com/wHAtAJ4.png"/>
                           </button>`;
-        div.setAttribute('class', 'list-item not-growing-padding');
+    div.setAttribute('class', 'list-item not-growing-padding');
 
-        element.appendChild(div);
-    }
+    element.appendChild(div);
+  }
 }
